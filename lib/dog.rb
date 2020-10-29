aaef5d0d7f98ca08eeecb06256c95c6c73aeaf75
Mimi-12 <oneiconforlife26@gmail.com>
2020-10-29 01:32:47 -0700

    commit tree 3cc77056c553b11cffeffdd97dd74e8a9b458f1e

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def initialize(name,breed="Mutt")
+      @name = name
+      @breed = breed
+    end
+    def name=(name)
+        @name = name
+      end
+    def breed=(breed)
+      @breed = Mutt
+    end
+    def name
+        @name
+      end
+    def breed
+      @breed
+    end
+  end 
